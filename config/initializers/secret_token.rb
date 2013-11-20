2402a84ee5dd45483f49b0cba8884238f1059f81
Morgan Garske <morgan.garske@colorado.edu>
2013-11-20 13:19:14 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DungeonHelper::Application.config.secret_key_base = 'c5ecee29c519c51bdf3c0e6f6487bedbc845acd0fa70a74f729f956e38ead7d9d4bff2302d1ddc4b91ee065815094528ae11b81eb26ef94fd7d18868f1b44c86'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DungeonHelper::Application.config.session_store :cookie_store, key: '_DungeonHelper_session'
