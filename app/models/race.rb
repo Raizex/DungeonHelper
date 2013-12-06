0ffc78d1eaa5401dcaae277bab08e07e7a6421ed
Morgan Garske <morgan.garske@colorado.edu>
2013-12-06 10:25:21 -0700

    added a method to race model that allows it to return a hash of its attribute modifiers for use in modifying the attribute table

--- app/models/race.rb
+++ app/models/race.rb
@@ -1,3 +1,14 @@
 class Race < ActiveRecord::Base
 	belongs_to :character
+
+	def attributeModifiers 
+		attrMods = Hash.new
+		attrStrings = @attribute_modifiers.split(', ')
+		attrStrings.each do |mod|
+			modValue = mod.split(' ')
+			attrMods[modValue[0]] = modValue[1]
+		end
+		return attrMods
+	end
+
 end
