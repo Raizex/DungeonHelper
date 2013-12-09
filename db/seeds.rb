d585042ba4d89e6f7bcc8c6414de6f25e61d1fea
Morgan Garske <morgan.garske@colorado.edu>
2013-12-08 18:01:12 -0700

    seeded dwarf and elf races

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+races = [{:race_name => "Dwarf",
+		  :race_description => "Dwarves are a stoic but stern race, 
+		  					  ensconced in cities carved from the 
+		  					  hearts of mountains and fiercely determined 
+		  					  to repel the depredations of savage races like 
+		  					  orcs and goblins.",
+		  :size => "Medium",
+		  :attribute_modifiers => "CON +2, WIS +2, CHA -2",
+		  :default_languages => "Common, Dwarven",
+		  :available_languages => "Giant, Gnome, Goblin, Orc, Terran, Undercommon"},
+		 {:race_name => "Elf",
+		  :race_description => "The long-lived elves are children of 
+		  						the natural world, similar in many superficial 
+		  						ways to fey creatures, though with key differences.",
+		  :size => "Medium",
+		  :attribute_modifiers => "DEX +2, INT +2, CON -2",
+		  :default_languages => "Common, Elven",
+		  :available_languages => "Celestial, Draconic, Gnoll, Gnome, Goblin, Orc, Sylvan"},
+  	 ]
+
+races.each do |race|
+  Race.create!(race)
+end
